81e2b1a1d67447a50bd66d13b9602e1f5adb9b0e
antoniapuspan <65085166+antoniapuspan@users.noreply.github.com>
2021-02-08 23:14:52 +0100

    correction

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 document.getElementById("button").addEventListener("click", generateOutput);
-
-
 function generateOutput() {
     let name = "costi forza";
     let capName = capitalizeFirstLetter(name);
-
     //find first name
     var fullName = "Paul Steve Panakkal".split(' '),
     firstName = fullName[0],
     lastName = fullName[fullName.length - 1];
     console.log(firstName);
+
+
 }
 // capitalize first letter
 function capitalizeFirstLetter(x) {
